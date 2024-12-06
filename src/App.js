19730a314a670fdc7cd65e2b12019c9c5fdea188
samorobo <101519233+samorobo@users.noreply.github.com>
2024-12-06 22:09:57 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     setPlaylist([]); // Clear playlist during loading
     try {
       const response = await axios.get(
-        `http://localhost:5000/api/playlist/${genre}`
+        `https://backend-ne7e.onrender.com/ ${genre}`
       );
       setPlaylist(response.data.playlist);
     } catch (err) {
